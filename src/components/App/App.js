2972d68fc76494b897e8dc9af3f61d346b549930
valentin gordienko <valentingordienko.dev@yandex.ru>
2020-05-28 01:23:13 +0300

    Реализация первичной структуры приложения

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SunWidget from "../SunWidget";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <SunWidget/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SunWidget/SunWidget.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./SunWidget.css";
+
+const mainCssClass = 'sun-widget';
+
+function SunWidget ({}) {
+
+
+	return <div className={mainCssClass}>
+
+	</div>
+}
+
+export default SunWidget;
