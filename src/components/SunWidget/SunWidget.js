477cc41ff0e3eef94bc346df20f4443f4eee9ea6
valentin gordienko <valentingordienko.dev@yandex.ru>
2020-06-02 02:27:20 +0300

    Реализация компонента SunWidget

--- src/components/SunWidget/SunWidget.js
+++ src/components/SunWidget/SunWidget.js
@@ -1,15 +1,38 @@
-import React from "react";
-
+import React, {memo} from "react";
+import {secondsToHms} from "../../utils";
+import Button from "../Button";
 import "./SunWidget.css";
 
-const mainCssClass = 'sun-widget';
+const mainCssClass = "sun-widget";
 
-function SunWidget ({}) {
-
+function SunWidget ({dateSunData, buttons, onDayNavigate}) {
+	const {date, sunrise, sunset, day_length} = dateSunData;
+	const dayLengthData = day_length ? secondsToHms(day_length) : null;
+	const sunriseData = sunrise ? new Date(sunrise) : null;
+	const sunsetData = sunset ? new Date(sunset) : null;
+	const formattedDate = date ? `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}` : '---';
+	const formattedSunrise = sunriseData ? `${sunriseData.getHours()}:${sunriseData.getMinutes()}:${sunriseData.getSeconds()}` : '---';
+	const formattedSunset = sunsetData ? `${sunsetData.getHours()}:${sunsetData.getMinutes()}:${sunsetData.getSeconds()}` : '---';
+	const formattedLength = dayLengthData ? `${dayLengthData.hours}:${dayLengthData.minutes}:${dayLengthData.seconds}` : '---';
 
 	return <div className={mainCssClass}>
-
+		<div className={`${mainCssClass}__item ${mainCssClass}__day-data`}>
+			<div>Day: {formattedDate}</div>
+			<div>Sunrise: {formattedSunrise}</div>
+			<div>Sunset: {formattedSunset}</div>
+			<div>Length: {formattedLength}</div>
+		</div>
+		<div className={`${mainCssClass}__item ${mainCssClass}__buttons`}>
+			{buttons.map((item)=>{
+				return <Button
+					key={item.id}
+					className={`${mainCssClass}__button`} caption={item.caption}
+					id={item.id}
+					onClick={onDayNavigate}
+				/>
+			})}
+		</div>
 	</div>
 }
 
-export default SunWidget;
+export default memo(SunWidget);
---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+export {secondsToHms} from "./secondsToHms";
